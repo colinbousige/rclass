932e24a58d43fe9a9ec1a32e333007ff7aca0750
Colin Bousige <colin.bousige@gmail.com>
2020-04-30 16:09:56 +0200

    ajout upload

---
+++ upload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp -r Data/* _book/Data/
+cp -r Plots/* _book/Plots/
+cp -r Exo/* _book/Exo/
+
+lftp -e "mirror -eRv _book/ lmi.cnrs.fr/www/r/; quit;" -u lab0958,"p|3lYCs8+" sftp://195.220.198.90:50029
+echo ""
+echo "Done."
+echo ""
+
+rm -rf ~/Travail/SiteLMI/SiteLMI/static/r/*
+cp -r _book/* ~/Travail/SiteLMI/SiteLMI/static/r/
