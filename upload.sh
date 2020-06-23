e7945d3a0ab6fe340e3d8be99812c505b3691e44
Colin Bousige <colin.bousige@gmail.com>
2020-06-23 08:52:22 +0200

    modif ordre

--- upload.sh
+++ upload.sh
@@ -3,11 +3,11 @@
 cp -r Data/* _book/Data/
 cp -r Plots/* _book/Plots/
 cp -r Exo/* _book/Exo/
+rm -rf ~/Travail/SiteLMI/SiteLMI/static/r/*
+cp -r _book/* ~/Travail/SiteLMI/SiteLMI/static/r/
 
 lftp -e "mirror -eRv _book/ lmi.cnrs.fr/www/r/; quit;" -u lab0958,"p|3lYCs8+" sftp://195.220.198.90:50029
 echo ""
 echo "Done."
 echo ""
 
-rm -rf ~/Travail/SiteLMI/SiteLMI/static/r/*
-cp -r _book/* ~/Travail/SiteLMI/SiteLMI/static/r/
