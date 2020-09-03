64e90ece7fd40455b8b63114cf6123280933c7c4
Colin Bousige <colin.bousige@gmail.com>
2020-09-03 14:24:48 +0200

    remove useless file

--- untitled.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-data = np.loadtxt("/Users/colin/Travail/Enseignements/R/Data/rubis_01.txt")
-plt.plot(data[:,0], data[:,1])
-plt.xlabel("w")
-plt.wlabel("Intensity")
-plt.show()
