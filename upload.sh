4a51cec5473fc6ec57d0d23b96ecff80dc62a734
Colin Bousige <colin.bousige@gmail.com>
2020-09-03 14:21:03 +0200

    ajout Rmd exo et google analytics

--- upload.sh
+++ upload.sh
@@ -2,7 +2,14 @@
 
 cp -r Data/* _book/Data/
 cp -r Plots/* _book/Plots/
-cp -r Exo/* _book/Exo/
+cp -r Exo/Data _book/Exo/
+cp -r Exo/Data_Religion _book/Exo/
+rmdremove=`ls _book/Exo/*.html`
+for file in rmdremove; do 
+    rm -f _book/Exo/${file//.html/}.Rmd
+done
+rm -f _book/Exo/*_solution.Rmd
+
 rm -rf ~/Travail/SiteLMI/SiteLMI/static/r/*
 cp -r _book/* ~/Travail/SiteLMI/SiteLMI/static/r/
 
