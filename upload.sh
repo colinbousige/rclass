b5debbfcc93dea7321557844efe61ea44b4ec02d
Colin Bousige <colin.bousige@gmail.com>
2020-09-21 15:35:21 +0200

    ajout correction spectro

--- upload.sh
+++ upload.sh
@@ -9,7 +9,15 @@ for file in rmdremove; do
     rm -f _book/Exo/${file//.html/}.Rmd
 done
 rm -f _book/Exo/*_solution.Rmd
-cp -r Exo/Rexercises-plot0_solution.Rmd _book/Exo/
+cp -r Exo/Rexercises-plot0_solution* _book/Exo/
+cp -r Exo/Rexercises-spectro_solution* _book/Exo/
+# cp -r Exo/Rexercises-tga_solution* _book/Exo/
+# cp -r Exo/Rexercises-co2_solution* _book/Exo/
+# cp -r Exo/Rexercises-covid_solution* _book/Exo/
+# cp -r Exo/Rexercises-Grt_solution* _book/Exo/
+# cp -r Exo/Rexercises-religion_babies_solution* _book/Exo/
+
+
 
 rm -rf ~/Travail/SiteLMI/SiteLMI/static/r/*
 cp -r _book/* ~/Travail/SiteLMI/SiteLMI/static/r/
